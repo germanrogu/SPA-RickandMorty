24ca3c9e63eb5b1457fb7d33efb384ffe4d09b10
German <german.rodriguezgu@gmail.com>
2022-03-11 22:53:40 -0500

    :sparkles: :lipstick: :hammer: Cards para los sobres y atomo boton

--- src/components/pages/GetCards.jsx
+++ src/components/pages/GetCards.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Card } from "../ui/atoms/Card/Card";
 
 export const GetCards = () => {
   return (
-    <div>GetCards</div>
-  )
-}
+    <>
+      <div>GetCards</div>
+      <Card titleCard={"Pack 1"} />
+    </>
+  );
+};
