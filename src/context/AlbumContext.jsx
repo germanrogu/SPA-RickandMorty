e5ed3f7d8da5321613d4bcd5795a8c696f97e895
German <german.rodriguezgu@gmail.com>
2022-03-12 23:51:34 -0500

    :zap: Funciones de agregar oersonaje a album y comprobar si esta repetido

--- src/context/AlbumContext.jsx
+++ src/context/AlbumContext.jsx
@@ -3,14 +3,26 @@ import { createContext, useState } from "react";
 export const AlbumContext = createContext();
 
 export const AlbumProvider = ({ children }) => {
-    const [albumArray, setAlbumArray] = useState([]);
+  const [albumArray, setAlbumArray] = useState([]);
+  const [repeatedCharacter, setRepeatedCharacter] = useState(false);
 
+  const addToAlbum = (character) => {
+    if (isInAlbum(character.id)) {
+      setRepeatedCharacter(true);
+    } else {
+      setRepeatedCharacter(false);
+      const newItem = {
+        character,
+      };
+      setAlbumArray([...albumArray, newItem]);
+    }
+  };
 
+  const isInAlbum = (characterId) => {
+    return albumArray.some((element) => element.character.id === characterId);
+  };
 
-
-
-  const value = {};
-
+  const value = { albumArray, addToAlbum, isInAlbum, repeatedCharacter };
 
   return (
     <AlbumContext.Provider value={value}>{children}</AlbumContext.Provider>
