410acff11478b47fbbde97c5d74c0e2b38ffba7e
German <german.rodriguezgu@gmail.com>
2022-03-12 23:52:59 -0500

    :lipstick: :zap: :art:  mejora visual y de conteindo en las cards

--- src/components/ui/atoms/Sheet/Sheet.jsx
+++ src/components/ui/atoms/Sheet/Sheet.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "./Sheet.module.css";
 
-export const Sheet = ({ image, name, species, location }) => {
+export const Sheet = ({ image, name, species, location ,onClick }) => {
   const BackgroundHead = {
     backgroundImage: `url(${image})`,
   };
 
   return (
     <div className={styles.center}>
-      <div className={styles.property_card}>
+      <div onClick={onClick} className={styles.property_card}>
         <div className={styles.property_image} style={BackgroundHead}></div>
         <div className={styles.property_description}>
           <h5> {name} </h5>
