5341dc81dd09af0d3b51bb7e83516ad24d88d657
German <german.rodriguezgu@gmail.com>
2022-03-12 23:53:30 -0500

    :sparkles:

--- src/components/ui/molecules/EpisodeSheetContainer/EpisodeSheetContainer.jsx
+++ src/components/ui/molecules/EpisodeSheetContainer/EpisodeSheetContainer.jsx
@@ -1,18 +1,16 @@
 import { Grid } from "@mui/material";
 import React from "react";
-import { Sheet } from "../../atoms/Sheet/Sheet";
+import { EpisodeSheet } from "../../atoms/EpisodeSheet/EpisodeSheet";
 
-export const SheetContainer = ({ items, onClick }) => {
+export const EpisodeSheetContainer = ({ items }) => {
   return (
     <Grid container spacing={1}>
       {items.map((item) => (
         <Grid item xs={12} md={3} key={item.id}>
-          <Sheet
-            image={item.image}
+          <EpisodeSheet
             name={item.name}
-            species={item.species}
-            location={item.location}
-            onClick={() => onClick(item)}
+            episode={item.episode}
+            air_date={item.air_date}
           />
         </Grid>
       ))}
