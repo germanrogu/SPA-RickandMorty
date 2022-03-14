f7055d7d2ca56c89b30ad67a1df6a5d60af197de
German <german.rodriguezgu@gmail.com>
2022-03-13 20:20:27 -0500

    :sparkles: Pagina de album mejorada

--- src/components/pages/MyAlbum.jsx
+++ src/components/pages/MyAlbum.jsx
@@ -1,13 +1,14 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AlbumContext } from "../../context/AlbumContext";
 import { ButtonCustom } from "../ui/atoms/ButtonCustom/ButtonCustom";
 import { ContentPages } from "../ui/atoms/ContentPages/ContentPages";
+import { Sheet } from "../ui/atoms/Sheet/Sheet";
 
 export const MyAlbum = () => {
   const { albumArray } = useContext(AlbumContext);
-  console.log(albumArray)
+
   return (
     <ContentPages>
       <Typography
@@ -24,6 +25,23 @@ export const MyAlbum = () => {
         {" My Album "}
       </Typography>
 
+      <Grid
+        container
+        spacing={3}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        {albumArray.map((item) => (
+          <Grid item xs={12} md={3} key={item.character.id}>
+            <Sheet
+              image={item.character.image}
+              name={item.character.name}
+              species={item.character.species}
+              location={item.character.location}
+            />
+          </Grid>
+        ))}
+      </Grid>
+
       <Typography
         style={{
           display: "flex",
@@ -33,19 +51,24 @@ export const MyAlbum = () => {
           fontSize: "1.2rem",
         }}
       >
-        Empty album
+        {albumArray.length > 0
+          ? "These are the sheets you have"
+          : "Empty Album"}
       </Typography>
-      <Link
-        to={`/GetCards`}
-        style={{
-          display: "flex",
-          textDecoration: "none",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <ButtonCustom>Go to Get cards</ButtonCustom>
-      </Link>
+
+      <Grid sx={{ display: "flex",justifyContent: "center"}}>
+        <Link
+          to={`/GetCards`}
+          style={{
+            display: "flex",
+            textDecoration: "none",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <ButtonCustom>Go to get more cards</ButtonCustom>
+        </Link>
+      </Grid>
     </ContentPages>
   );
 };
