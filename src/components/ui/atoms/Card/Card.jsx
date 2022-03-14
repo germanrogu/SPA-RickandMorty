4f1c922911f8465d30e3b0564170394d86085016
German <german.rodriguezgu@gmail.com>
2022-03-13 21:57:20 -0500

    :lipstick: Cambios en el car de los pack

--- src/components/ui/atoms/Card/Card.jsx
+++ src/components/ui/atoms/Card/Card.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { ButtonCustom } from "../ButtonCustom/ButtonCustom";
 import styles from "./Card.module.css";
+import rockAlbum from '../../../../img/RickAlbum.png'
 
 export const Card = ({ titleCard, onClick, disabled }) => {
+  const BackgroundHead = {
+    backgroundImage: `url(${rockAlbum})`,
+    
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={BackgroundHead}>
       <div className={styles.content}>
         <h2>{titleCard}</h2>
         <ButtonCustom disabled={disabled} onClick={onClick}>
