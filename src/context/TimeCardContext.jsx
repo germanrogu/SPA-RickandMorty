57c095be57277cc424211010af1d75a16dd5878a
German <german.rodriguezgu@gmail.com>
2022-03-13 20:19:35 -0500

    :zap: :art: Mejora en el comportamiento del timer

--- src/context/TimeCardContext.jsx
+++ src/context/TimeCardContext.jsx
@@ -1,10 +1,14 @@
 import { createContext, useEffect, useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export const TimeCardContext = createContext();
 
 export const TimeCardProvider = ({ children }) => {
-  const [isActive, setIsActive] = useState(false);
-  const [seconds, setSeconds] = useState(0);
+  // const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useLocalStorage(false, "isActive");
+
+  // const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useLocalStorage(0, "secondsTimer");
 
   const activeTimer = () => {
     setIsActive(true);
@@ -19,15 +23,13 @@ export const TimeCardProvider = ({ children }) => {
     let interval = null;
     if (isActive) {
       interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
+        setSeconds((seconds) => seconds + 1);
       }, 1000);
     } else if (!isActive && seconds !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
   }, [isActive, seconds]);
-
-
 
   const value = {
     activeTimer,
