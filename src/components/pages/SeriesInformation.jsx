3f3b74a5ad4949145df2cd40fb7fe6ab8578cf91
German <german.rodriguezgu@gmail.com>
2022-03-14 22:20:30 -0500

    Page nueva

--- src/components/pages/SeriesInformation.jsx
+++ src/components/pages/SeriesInformation.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
+import { ContentPages } from "../ui/atoms/ContentPages/ContentPages";
+import { TabsPanel } from "../ui/molecules/TabsPanel/TabsPanel";
 
 export const SeriesInformation = () => {
   return (
-    <div>SeriesInformation</div>
-  )
-}
+    <ContentPages>
+      <Typography
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          fontSize: "1.8rem",
+          fontWeight: "600",
+          color: "#722f37",
+          paddingTop: "1.3rem",
+          paddingBottom: "1.3rem",
+        }}
+      >
+        {"Series Information"}
+      </Typography>
+
+      <TabsPanel itemOne={"1"} itemTwo={"2"} itemThree={"3"} />
+    </ContentPages>
+  );
+};
