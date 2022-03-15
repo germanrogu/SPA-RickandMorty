6c36a614bf59da732d99078cd55a27c3c958175b
German <german.rodriguezgu@gmail.com>
2022-03-14 23:50:35 -0500

    :construction: :zap: Logica para el llamado de la api , falta optimizat

--- src/components/pages/SeriesInformation.jsx
+++ src/components/pages/SeriesInformation.jsx
@@ -1,9 +1,74 @@
 import { Typography } from "@mui/material";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { ContentPages } from "../ui/atoms/ContentPages/ContentPages";
 import { TabsPanel } from "../ui/molecules/TabsPanel/TabsPanel";
 
 export const SeriesInformation = () => {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const [location, setLocation] = useState([]);
+  const [infoLocation, setInfoLocation] = useState({});
+
+  const url = "https://rickandmortyapi.com/api/character";
+  const urlLocation = "https://rickandmortyapi.com/api/location";
+
+  const fetchCharacters = (url) => {
+    axios
+      .get(url)
+      .then((data) => {
+        setCharacters(data.data.results);
+        setInfo(data.data.info);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleNextPage = () => {
+    fetchCharacters(info.next);
+    window.scrollTo(0, 0);
+  };
+
+  const handlePreviousPage = () => {
+    fetchCharacters(info.prev);
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    fetchCharacters(url);
+  }, []);
+
+  //-------------------------------------------------------------------//
+
+  const fetchLocation = (url) => {
+    axios
+      .get(url)
+      .then((data) => {
+        setLocation(data.data.results);
+        setInfoLocation(data.data.info);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const onClickNextLocation = () => {
+    fetchLocation(infoLocation.next);
+    window.scrollTo(0, 0);
+  };
+
+  const onClickPreviousLocation = () => {
+    fetchLocation(infoLocation.prev);
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    fetchLocation(urlLocation);
+  }, []);
+
+
   return (
     <ContentPages>
       <Typography
@@ -20,7 +85,20 @@ export const SeriesInformation = () => {
         {"Series Information"}
       </Typography>
 
-      <TabsPanel itemOne={"1"} itemTwo={"2"} itemThree={"3"} />
+      <TabsPanel
+        disabledPrevious={!info.prev}
+        disabledNext={!info.next}
+        onClickPrevious={handlePreviousPage}
+        onClickNext={handleNextPage}
+        itemsCharacters={characters}
+
+        disabledPreviousLocation={!infoLocation.prev}
+        disabledNextLocation={!infoLocation.next}
+        onClickPreviousLocation={onClickPreviousLocation}
+        onClickNextLocation={onClickNextLocation}
+        itemsLocation={location}
+        itemThree={"3"}
+      />
     </ContentPages>
   );
 };
