804fbec7db25560139331645dde5fab684ded39f
German <german.rodriguezgu@gmail.com>
2022-03-15 22:23:47 -0500

    :hammer: Refactor para las funciones globales

--- src/components/ui/molecules/EpisodeSheetContainer/EpisodeSheetContainer.jsx
+++ src/components/ui/molecules/EpisodeSheetContainer/EpisodeSheetContainer.jsx
@@ -2,12 +2,17 @@ import { Grid } from "@mui/material";
 import React from "react";
 import { EpisodeSheet } from "../../atoms/EpisodeSheet/EpisodeSheet";
 
-export const EpisodeSheetContainer = ({ items }) => {
+export const EpisodeSheetContainer = ({ items, buttonAdd, onClick }) => {
+  const onClickChange = (item) => {
+    onClick(item, "episode");
+  };
   return (
     <Grid container spacing={1}>
       {items.map((item) => (
         <Grid item xs={12} md={3} key={item.id}>
           <EpisodeSheet
+            buttonAdd={buttonAdd}
+            onClick={() => onClickChange(item)}
             name={item.name}
             episode={item.episode}
             air_date={item.air_date}
--- src/components/ui/molecules/SheetContainer/SheetContainer.jsx
+++ src/components/ui/molecules/SheetContainer/SheetContainer.jsx
@@ -2,17 +2,21 @@ import { Grid } from "@mui/material";
 import React from "react";
 import { Sheet } from "../../atoms/Sheet/Sheet";
 
-export const SheetContainer = ({ items, onClick }) => {
+export const SheetContainer = ({ items, onClick, buttonAdd }) => {
+  const onClickChange = (item)=>{
+    onClick(item,'character')
+  }
   return (
     <Grid container spacing={1}>
       {items.map((item) => (
         <Grid item xs={12} md={3} key={item.id}>
           <Sheet
+            buttonAdd={buttonAdd}
             image={item.image}
             name={item.name}
             species={item.species}
             location={item.location}
-            onClick={() => onClick(item)}
+            onClick={() => onClickChange(item)}
           />
         </Grid>
       ))}
