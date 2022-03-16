1d5973b44f4bc6f892112d9f68fcd26228332deb
German <german.rodriguezgu@gmail.com>
2022-03-15 22:24:16 -0500

    :zap: :hammer: Codigo funional

--- src/components/ui/atoms/Sheet/Sheet.jsx
+++ src/components/ui/atoms/Sheet/Sheet.jsx
@@ -1,20 +1,47 @@
+import { Button, Tooltip } from "@mui/material";
 import React from "react";
 import styles from "./Sheet.module.css";
+import AddBoxIcon from "@mui/icons-material/AddBox";
 
-export const Sheet = ({ image, name, species, location ,onClick }) => {
+export const Sheet = ({
+  buttonAdd,
+  image,
+  name,
+  species,
+  location,
+  onClick,
+}) => {
   const BackgroundHead = {
     backgroundImage: `url(${image})`,
   };
 
   return (
     <div className={styles.center}>
-      <div onClick={onClick} className={styles.property_card}>
-        <div className={styles.property_image} style={BackgroundHead}></div>
+      <div className={styles.property_card}>
+        <div className={styles.property_image} style={BackgroundHead}>
+          <Tooltip title={buttonAdd === "card" ? "Add" : "View"}>
+            <Button
+              onClick={onClick}
+              disableElevation={true}
+              size="medium"
+              style={{
+                borderRadius: "8px",
+                fontWeight: "bold",
+                color: "black",
+                backgroundColor: "#DBF227",
+                textTransform: "none",
+              }}
+              startIcon={<AddBoxIcon />}
+            >
+              {buttonAdd === "card" ? "Add" : "View"}
+            </Button>
+          </Tooltip>
+        </div>
         <div className={styles.property_description}>
           <h5> {name} </h5>
           <p>Species: {species}</p>
           <p>Location: {location.name}</p>
-          <p>Category: {'Character'}</p>
+          <p>Category: {"Character"}</p>
         </div>
       </div>
     </div>
